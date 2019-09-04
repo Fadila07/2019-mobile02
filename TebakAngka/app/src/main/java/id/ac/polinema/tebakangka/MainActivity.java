31097b53a98229fab061c43e5f2464fe95bfd81e
Fadila07 <rfadila195@gmail.com>
2019-09-04 17:45:15 +0700

    membuat generate random number

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,40 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText numberInput;
+	int angka;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		numberInput = findViewById(R.id.number_input);
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-
+		angka = (int) (1+(Math.random()*100));
+//		Random rnd = new Random();
+//		angka = rnd.nextInt(100) + 1;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		String input = numberInput.getText().toString();
+		int number = Integer.parseInt(input);
+		if(number == angka){
+			Toast.makeText(this, "Tebakan Anda Benar!", Toast.LENGTH_SHORT).show();
+		} else if(number < angka){
+			Toast.makeText(this, "Tebakan Anda Lebih Kecil!", Toast.LENGTH_SHORT).show();
+		} else if(number > angka){
+			Toast.makeText(this, "Tebakan Anda Lebih Besar!", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		numberInput.setText("");
+		initRandomNumber();
 	}
 }
